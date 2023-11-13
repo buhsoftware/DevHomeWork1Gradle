9ba1d5275f660b15f6c01ca7181255417d270e3b
buhsoftware <110223030+buhsoftware@users.noreply.github.com>
2023-11-14 00:21:17 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "UserTest"
+    val dependencies = configurations
+            .runtimeClasspath
+            .get()
+            .map(::zipTree) // OR .map { zipTree(it) }
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
